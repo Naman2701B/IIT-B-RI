6511040d95bdae1ee4f059e6de299d02e0be3916
AryaVrath <107951856+OnamS@users.noreply.github.com>
2024-06-01 10:25:00 +0530

    TA LFA Done

--- timetableoutput.py
+++ timetableoutput.py
@@ -13,38 +13,41 @@ def timeTableExcel(basefolderinput):
     for i in range(0, len(df)):
         if (df[0][i] == "TrainNumber "):
             rows.append(i)
-    rows.append(len(df)+1)    
-    for j in range(1, len(rows)):
+    for j in range(0, len(rows)-1):
         stationName = []
         stationNumber =[]
         dwellTime = []
+        departureTime = [df[2][rows[j]+1]+df[5][0]]
         distanceBetweenStations = []
-        timeFromStarting = [df[2][rows[j-1]+1]]
+        timeFromStarting = [df[2][rows[j]+1]]
         timeFromStarting_mins = []
-        values = df[2][rows[j-1]+1].split(":")
-        for k in range(rows[j-1]+4, rows[j]-1):
+        for k in range(rows[j]+4, rows[j+1]-1):
             stationName.append(df[1][k])
             stationNumber.append(df[2][k])
             dwellTime.append(df[5][k])
-            if(k == rows[j-1]+4):
+            if(k == rows[j]+4):
                 continue
             else:
                 distanceBetweenStations.append(abs(int(df[3][k])-int(df[3][k-1])))
             timeFromStarting_mins.append(int(df[4][k]))
-        temp = str(timedelta(hours=int(values[0]), minutes=int(values[1]))+timedelta(hours=(int(dwellTime[0])//60), minutes=(int(dwellTime[0])%60)))
-        departureTime = [temp]
         for i in range(0, len(timeFromStarting_mins)):
+            values = df[2][rows[j]+1].split(":")
             h = int(timeFromStarting_mins[i]) // 60
             m = int(timeFromStarting_mins[i]) % 60
             timeFromStarting.append(str(timedelta(
                 hours=h, minutes=m) + timedelta(hours=int(values[0]), minutes=int(values[1]))))
             departureTime.append(str(timedelta(
                 hours=h, minutes=m) +timedelta(hours=int(values[0]), minutes=int(values[1]))+timedelta(hours=(int(dwellTime[i-1])//60), minutes=(int(dwellTime[i-1])%60))))
+            # print(timeFromStarting_mins)
+            # print(dwellTime)
+            # print(timeFromStarting)
+            # print(departureTime)
         for i in range(0, len(timeFromStarting_mins)):
             timeFromStarting_mins[i] = timeFromStarting_mins[i] + \
-                int(datetime.strptime(df[2][rows[j-1]+1], "%H:%M").minute)
-        temp = {"trainnumber": df[0][rows[j-1]+1], "startDistance": df[3][rows[j-1]+4],
-                "trainType": df[6][rows[j-1]+1],
+                int(datetime.strptime(df[2][rows[j]+1], "%H:%M").minute)
+        # print(timeFromStarting_mins)
+        temp = {"trainnumber": df[0][rows[j]+1], "startDistance": df[3][rows[j]+4],
+                "trainType": df[6][rows[j]+1],
                 "stationNameToDisplay": station_name,
                 "stationNumber":stationNumber,
                 "distanceBetweenStations": distanceBetweenStations,
@@ -60,4 +63,7 @@ def timeTableExcel(basefolderinput):
                 final_dict[i]["timeFromStarting"].insert(j-1, "-")
                 final_dict[i]["dwellTime"].insert(j-1,"-")
                 final_dict[i]["departureTime"].insert(j-1,"-")
+    # print(final_dict)
     return final_dict
+
+# timeTableExcel(basefolderinput)
